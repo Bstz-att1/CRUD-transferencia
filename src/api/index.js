48ddc89fadc9f110b657992713448b7b087bf948
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-04-18 10:44:21 -0500

    refactor(api): actualizar índice para exponer todos los métodos de tareas y usuarios
    - Se centralizaron las exportaciones de ambos módulos de API.
    - Se estableció un punto de entrada único para simplificar las importaciones desde la capa de servicios.

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,19 @@ export {
     taskPost,
     taskGet,
     taskGetByUser,
+    taskPut,
     taskPatch,
     taskDelete
 } from './tasks.api.js';
+
+// ======================================================================
+//                     EXPORTACIONES | USERS.API.JS
+// ======================================================================
+export {
+    userGet,
+    userGetById,
+    userPost,
+    userPut,
+    userPatch,
+    userDelete
+} from './users.api.js';
